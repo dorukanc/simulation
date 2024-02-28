1f2a70ebd30c5b61a1640c24a5b14478de2cd1c3
dorukanc <catakd@mef.edu.tr>
2024-02-28 16:36:13 +0300

    next departure time function added

--- parkingspot.py
+++ parkingspot.py
@@ -16,3 +16,11 @@ def create_arrival(time):
     
     next_arrival_time = time + random.expovariate(lambda_t)
     return next_arrival_time
+
+# avarage_service_time = 2 hours
+avarage_service_time = 2
+
+def create_departure(time):
+    labmda_ast = 1/avarage_service_time
+    next_departure_time = time + random.expovariate(labmda_ast)
+    return next_departure_time
