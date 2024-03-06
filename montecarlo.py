abeda50bdae997be39cac02b31f5471ca92cbe64
dorukanc <catakd@mef.edu.tr>
2024-03-06 21:03:32 +0300

    matplotlib added, graph of points inside of the circle shown

--- montecarlo.py
+++ montecarlo.py
@@ -1,8 +1,12 @@
 import random
+import matplotlib.pyplot as plt
 
 interval = 1000
 circle_points = 0
 square_points = 0
+
+points_inside_circlex = []
+points_inside_circley = []
 
 
 for i in range(interval):
@@ -13,8 +17,25 @@ for i in range(interval):
 
     if origin_distance <= 1:
         circle_points += 1
+        points_inside_circlex.append(rnd_x)
+        points_inside_circley.append(rnd_y)
     square_points += 1
 
     pi = 4 * circle_points / square_points
 
 print("Estimated pi value through simulation: ", pi)
+
+#Plotting square boundary
+plt.plot([-1, 1], [1, 1], color='red')  # Top line
+plt.plot([-1, 1], [-1, -1], color='red')  # Bottom line
+plt.plot([-1, -1], [-1, 1], color='red')  # Left line
+plt.plot([1, 1], [-1, 1], color='red')  # Right line
+
+# Plotting points inside the circle
+plt.scatter(points_inside_circlex, points_inside_circley, color='blue', label='points inside circle')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('Points inside circle')
+plt.legend()
+plt.axis('equal')
+plt.show()
