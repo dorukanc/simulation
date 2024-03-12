7997e6ff87fd8485176dd07b982b5312c79298c3
dorukanc <catakd@mef.edu.tr>
2024-03-12 20:06:01 +0300

    pdf of the normal ditrubition, found the estimated area under the function

--- py/areaunderfunc_sim.py
+++ py/areaunderfunc_sim.py
@@ -1,39 +1,50 @@
 import random
 import matplotlib.pyplot as plt
+import math
 
-interval = 1000
+# PDF of the normal distrubition
+def func(x):
+    return math.exp(-x ** 2 / 2) / math.sqrt(2 * math.pi)
+
+
+interval = 10000000
 under_func_points = 0
-rectangle_points = 0
 
 points_underfunc_x = []
 points_underfunc_y = []
+
+# define a b c values
+a = -2
+b = 2
+c = 0.4
+
+area_rectangle = (b - a) * c
 
 # generate random points inside rectangle where x is between a and b
 # y is between 0 and c
 for i in range(interval):
 
-    # Generate rnd_x between 3 and 7
-    rnd_x = 3 + (7 - 3) * random.random()
-    points_underfunc_x.append(rnd_x)
-    # Generate rnd_y between 0 and 0.45
-    rnd_y = 0 + (0.45 - 0) * random.random()
-    points_underfunc_y.append(rnd_y)
+    # Generate rnd_x between 0 and 2
+    rnd_x = a + (b - a) * random.random()
+    # Generate rnd_y between 0 and 1
+    rnd_y = 0 + c * random.random()
 
+    if rnd_y <= func(rnd_x):
+        under_func_points += 1
+        points_underfunc_x.append(rnd_x)
+        points_underfunc_y.append(rnd_y)
 
-# Plotting square boundary with adjusted rectangle boundaries
-plt.plot([3, 7], [0.45, 0.45], color='red')  # Top line
-plt.plot([3, 7], [0, 0], color='red')  # Bottom line
-plt.plot([3, 3], [0, 0.45], color='red')  # Left line
-plt.plot([7, 7], [0, 0.45], color='red')  # Right line
+    ratio_in = under_func_points / interval
+    estimated_area = ratio_in * area_rectangle
+
+print("Estimated area under the function through simulation is:", estimated_area)
 
 # Adding labels and title
 plt.xlabel('X')
 plt.ylabel('Y')
-plt.title('Rectangle Boundaries')
+plt.title('Points under the function')
 
 # Plotting points under the function
-plt.scatter(points_underfunc_x, points_underfunc_y, color='blue', label='points under the rectangle')
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.title('Points under the function')
+plt.scatter(points_underfunc_x, points_underfunc_y, color='blue', label='Points under the function')
+plt.legend()
 plt.show()
