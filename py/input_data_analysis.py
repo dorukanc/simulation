2102d306c0337530f0c11a1eb250ff999f1a8bb3
dorukanc <catakd@mef.edu.tr>
2024-03-27 15:54:41 +0300

    input_data_anaylsis.py added

---
+++ py/input_data_analysis.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import stats
+import pandas as pd
+
+
+df = pd.read_excel('../data/data.xlsx')
+df.head()
+
+# Histogram of the dataset
+
+bin_sizes = [5, 10, 20, 25, 50]
+
+# Create subplots
+fig, axs = plt.subplots(1, 5, figsize=(16,5))
+
+
+for i, bins in enumerate(bin_sizes):
+    axs[i].hist(df['data1'], bins=bins, color='skyblue', edgecolor='black')
+    axs[i].set_title(f'Bin size = {bins}')
+    axs[i].set_xlabel('Data 1')
+    axs[i].set_ylabel('Frequency')
+
+# Adjust layout
+plt.tight_layout()
+
+# Display histograms
+
+plt.show()
+
+
+
